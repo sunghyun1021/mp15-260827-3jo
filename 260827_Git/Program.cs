4eb865371b59a2d41c7cb44cf0e1bcddeb4c135e
sunghyun1021 <sy777choi@naver.com>
2026-08-31 09:56:53 +0900

    [Feat] IntroduceSunghyun1021 함수 추가
    Closes #1

--- 260827_Git/Program.cs
+++ 260827_Git/Program.cs
@@ -1,1 +1,20 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Program  
+{  
+    public static void Main()  
+    {  
+        Console.WriteLine("=== N조 팀원 소개 ===");  
+        // 여기에 팀원 소개 호출을 한 줄씩 추가합니다
+        
+        IntroduceSunghyun1021();
+
+        Console.WriteLine("=== 소개 끝 ===");  
+    }
+
+    // 여기에 팀원 소개 메서드를 하나씩 추가합니다  
+    public static void IntroduceSunghyun1021()
+    {
+
+    }
+}
